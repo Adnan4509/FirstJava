c52ed1a542843f0167fd92d61d417a2708b82606
Adnan4509 <adnanwebdev7@gmail.com>
2025-07-18 22:07:40 +0530

    completed 7 patterns in Java

---
+++ Demo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("hey " + x);
+        System.out.println("hello world");
+
+        sc.close();
+    }
+} 
