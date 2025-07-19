a5c5a55e36a963aede5c9248895bf07c6785291c
Adnan4509 <adnanwebdev7@gmail.com>
2025-07-19 14:54:04 +0530

    pattern 11 completed

--- patterns.java
+++ patterns.java
@@ -112,13 +112,35 @@ public class patterns {
             System.out.println();
         }
     }
+
+    public static void pattern11(int n){
+        int space = 2*(n-1);
+        for (int i=1; i<=n; i++){
+            // numbers
+                for(int j=1; j<=i; j++){
+                    System.out.print(j);
+                }
+            // spaces
+                for(int j=1; j<space; j++){
+                    System.out.print(" ");
+                }
+            // numbers
+                for(int j=i; j>=1; j--){
+                    System.out.print(j);
+                }
+                System.out.println();
+                space -= 2;
+        }
+    }
+
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
         for (int i = 0; i < t; i++) {
             int n = sc.nextInt();
-            pattern10(n);
+            pattern11(n);
         }
         sc.close();
 
