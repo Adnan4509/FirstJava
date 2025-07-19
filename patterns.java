9cc0a98084207196fcf298b56bae6ae79d0dbbd3
Adnan4509 <adnanwebdev7@gmail.com>
2025-07-19 15:07:03 +0530

    Completed pattern 13

--- patterns.java
+++ patterns.java
@@ -143,13 +143,22 @@ public class patterns {
             System.out.println();
         }
     }
+
+    public static void pattern13(int n){
+        for(int i=0; i<=n; i++){
+            for(char j='A'; j< 'A'+i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
         for (int i = 0; i < t; i++) {
             int n = sc.nextInt();
-            pattern12(n);
+            pattern13(n);
         }
         sc.close();
 
