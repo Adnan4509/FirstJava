74aa9d453af020397e2de23513d77c773a531603
Adnan4509 <adnanwebdev7@gmail.com>
2025-07-19 15:00:13 +0530

    competed pattern 12

--- patterns.java
+++ patterns.java
@@ -133,14 +133,23 @@ public class patterns {
         }
     }
 
-    
+    public static void pattern12(int n){
+        int num = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(num + " ");
+                num = num + 1;
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
         for (int i = 0; i < t; i++) {
             int n = sc.nextInt();
-            pattern11(n);
+            pattern12(n);
         }
         sc.close();
 
