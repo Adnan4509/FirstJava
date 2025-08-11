2c07e7a786f59b5fb652fdb5960d090542ca8fce
Adnan4509 <adnanwebdev7@gmail.com>
2025-08-11 23:26:02 +0530

    Abstraction in java

--- Demo.java
+++ Demo.java
@@ -1,12 +1,29 @@
-import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        System.out.println("hey " + x);
-        System.out.println("hello world");
-
-        sc.close();
+        Dog dog = new Dog();
+        dog.sayBye();
+        dog.sayHello();
     }
 } 
+
+abstract class Animal{
+   abstract void sayHello();
+   abstract void sayBye();
+
+   void sleep(){
+    System.out.println("ghur");
+   }
+}
+
+
+class Dog extends Animal {
+    
+    void sayHello(){
+        System.out.println("woof");
+    }
+
+    void sayBye(){
+        System.out.println("Hoof");
+    }
+}
