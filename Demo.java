e3c6b84384b959790f033671ffe5ba4f3ca06324
Adnan4509 <adnanwebdev7@gmail.com>
2025-08-12 00:04:28 +0530

    constructor

--- Demo.java
+++ Demo.java
@@ -1,46 +1,49 @@
 
 public class Demo {
-    public static void main(String[] args){
-        int[] a = {1,2,3,4,5,6,7};
-        a[4] = 34;
-        for(int i=0; i<a.length; i++){        //for loop to print array element
-            System.out.println(a[i]);
-        }
-        for (int i : a) {
-            System.out.println(i);          //for each loop print array elements
-        }
+    public static void main(String[] args) {
+        Dog dog = new Dog("black");
+        dog.speed = 110;
+
+        System.out.println(dog.speed);
+        System.out.println();
     }
-} 
-
-abstract class Animal{
-   abstract void sayHello();
-   abstract void sayBye();
-
-   void sleep(){
-    System.out.println("ghur");
-   }
 }
 
+abstract class Animal {
+    abstract void sayHello();
+
+    abstract void sayBye();
+
+    void sleep() {
+        System.out.println("ghur");
+    }
+}
 
 class Dog extends Animal {
-    
-    void sayHello(){
+    private String colour;
+    int speed;
+
+    public Dog(String colour){                  //here we are using a constructor to access private value for once
+        this.colour = colour;
+    }
+
+    void sayHello() {
         System.out.println("woof");
     }
 
-    void sayBye(){
+    void sayBye() {
         System.out.println("Hoof");
     }
 }
 
-interface Mobile{
+interface Mobile {
     void makeCall();
-    
+
 }
 
-class SmartPhone implements Mobile{
+class SmartPhone implements Mobile {
 
-   public void makeCall(){                        // cannot declare method without public keyword
+    public void makeCall() { // cannot declare method without public keyword
         System.out.println("making call");
     }
 }
